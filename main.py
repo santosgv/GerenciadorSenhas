bd3ad5850e29ff2e67ae0200cc0b4a7fba1b7ac1
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2021-02-10 11:08:26 -0300

    implementando a exibiçao de senha no db

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from db import *
 from tkinter import *
 from tkinter import ttk
+from db import Banco
 
 root=Tk()
 login=('vitor','senha123@')
-
-
 
 class Funcoes():
     def validar(self):
@@ -21,18 +20,50 @@ class Funcoes():
             self.lbaddsenha = Label(self.Main, text='Adicionar novas senhas')
             self.lbaddsenha.place(relx=0.01, rely=0.35)
 
-            self.btadd=Button(self.Main,text='Adicionar')
+            self.btadd=Button(self.Main,text='Adicionar',command=self.addsenha)
             self.btadd.place(relx=0.45, rely=0.35)
 
             self.lbverificar = Label(self.Main, text='Verificar senhas salvas')
             self.lbverificar.place(relx=0.01, rely=0.45)
 
-            self.btshow=Button(self.Main,text='Mostrar senhas')
+            self.btshow=Button(self.Main,text='Mostrar senhas',command=self.showsenha)
             self.btshow.place(relx=0.45, rely=0.45)
 
+    def addsenha(self):
+        self.btshow.destroy()
+        self.lbverificar.destroy()
+        self.btadd.destroy()
+        self.lbaddsenha.destroy()
 
+        self.lbsite=Label(self.Main,text='Sites')
+        self.lbsite.place(relx=0.1,rely=0.1)
 
+        self.lbsenhas=Label(self.Main,text='Senhas')
+        self.lbsenhas.place(relx=0.6, rely=0.1)
 
+    def showsenha(self):
+        self.btshow.destroy()
+        self.lbverificar.destroy()
+        self.btadd.destroy()
+        self.lbaddsenha.destroy()
+
+        self.showsenhas=Label(self.Main,text='Mostrando senhas')
+        self.showsenhas.place(relx=0.1, rely=0.1)
+
+        self.checksenhasite=Entry(self.Main)
+        self.checksenhasite.place(relx=0.1, rely=0.20)
+
+        self.btexibir=Button(self.Main,text='Exibir',command=self.senhadb)
+        self.btexibir.place(relx=0.40, rely=0.20)
+
+    def senhadb(self):
+        b=Banco()
+        b.conecta()
+        site=self.checksenhasite.get()
+        senhasdobanco =str(b.checksenha(site))
+        self.lbsenhasbanco = Label(self.Main,text='')
+        self.lbsenhasbanco.place(relx=0.1, rely=0.30,relwidth=0.60,relheight=0.60)
+        self.lbsenhasbanco['text']=str(senhasdobanco)
 
 
 
@@ -69,7 +100,5 @@ class Aplicacao(Funcoes):
 
         self.btloga=Button(self.Main,text='Logar',command=self.validar)
         self.btloga.place(relx=0.50, rely=0.55)
-
-
 
 Aplicacao()
